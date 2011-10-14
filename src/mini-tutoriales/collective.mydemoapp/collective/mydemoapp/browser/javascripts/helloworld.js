6e067b36d9efc7db48d596eae73bed8b8b52b064
macagua = Leonardo J .Caballero G <leonardocaballero@gmail.com>
2011-10-14 23:51:36 +0000

    Added JS Directory and helloword.js file

---
+++ src/mini-tutoriales/collective.mydemoapp/collective/mydemoapp/browser/javascripts/helloworld.js
@@ -0,0 +1,8 @@
+function helloworld() {
+  var message = ""
+  // adde you name
+  message = prompt("Please, enter your name");
+  // Show your greeting with the name
+  alert("Hello " + message);
+}
+
